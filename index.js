d5c2662d70b53c46a0116ee3b5bb4fb3555b7dbc
Sam Hollyer <rsamhollyer@gmail.com>
2021-05-18 18:34:12 -0500

    working on db

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-const http = require('http');
-const express = require('express');
-const db = require('./db/db.js');
+// const cron = require('./lib/cron');
+const data = require('./db/db.json');
 
-const app = express();
-const server = http.createServer(app);
-const studentData = require('./student_repos/data');
-const httpLogger = require('./logs');
-const go = require('./lib');
-
-const HOST = 'localhost';
-const PORT = 4834;
-
-app.use(httpLogger);
-
-app.get('/', (req, res, next) => {
-  res.send('HI');
-});
-
-app.get('/scrape', async (req, res, next) => {
-  console.log(`SCRAPING!`);
-  const students = await go(studentData);
-  db.get('students').assign(students).write();
-  res.json({ students });
-});
-
-server.listen(PORT, HOST, () => {
-  console.log(`App listening on http://${HOST}:${PORT}`);
-});
+console.log(data['Alyna Palamarchuk']);
