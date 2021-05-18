2401f1f96c44ce1a348200c10b1010125b201373
Sam Hollyer <rsamhollyer@gmail.com>
2021-05-18 14:53:46 -0500

    working on db

--- db/db.js
+++ db/db.js
@@ -3,5 +3,6 @@ const FileSync = require('lowdb/adapters/FileSync');
 
 const adapter = new FileSync('./db/db.json');
 const db = low(adapter);
+db.defaults({ students: {} }).write();
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,62 +1,17 @@
 const http = require('http');
 const express = require('express');
-const logger = require('morgan')('dev');
+const db = require('./db/db.js');
 
 const app = express();
 const server = http.createServer(app);
-const axios = require('axios');
-const cheerio = require('cheerio');
-const db = require('./db/db.js');
 const studentData = require('./student_repos/data');
-// db.get('students')
-//   .assign({ sam: [{}, {}, {}], count: 0 })
-//   .write();
+const httpLogger = require('./logs');
+const go = require('./lib');
 
 const HOST = 'localhost';
 const PORT = 4834;
-app.use(logger);
 
-async function getGithub(url) {
-  const { data: html } = await axios.get(url);
-  return html;
-}
-
-async function createObjects(student, cheerioLinks) {
-  const userObj = {};
-
-  userObj[student] = { repos: cheerioLinks, total: cheerioLinks.length };
-  return userObj;
-}
-async function getJournals(student, html) {
-  const $ = cheerio.load(html, false);
-  const links = $('[data-pjax="#repo-content-pjax-container"]');
-  const allLinks = [];
-  links.each((_, el) => {
-    allLinks.push({
-      text: $(el).text(),
-      href: $(el).attr('href'),
-    });
-  });
-
-  const userScrape = createObjects(student, allLinks);
-  return userScrape;
-}
-
-async function go(studentObject) {
-  const students = {};
-  for (const key of Object.keys(studentObject)) {
-    if (studentObject[key] === null) {
-      const nullData = {};
-      const name = `${key}`;
-      nullData[name] = null;
-      Object.assign(students, nullData);
-    } else {
-      const data = await getJournals(key, await getGithub(studentObject[key]));
-      Object.assign(students, data);
-    }
-  }
-  return students;
-}
+app.use(httpLogger);
 
 app.get('/', (req, res, next) => {
   res.send('HI');
@@ -64,8 +19,13 @@ app.get('/', (req, res, next) => {
 
 app.get('/scrape', async (req, res, next) => {
   console.log(`SCRAPING!`);
-  const stuff = await go(studentData);
-  res.json({ stuff });
+  const students = await go(studentData);
+  db.get('students')
+    .assign(students)
+    .last()
+    .assign({ updatedLast: Date.now().toString() })
+    .write();
+  res.json({ students });
 });
 
 server.listen(PORT, HOST, () => {
