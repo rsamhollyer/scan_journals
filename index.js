df04b3ca4685600e104f07b894b2d2ac6cd650e4
rsamhollyer <rsamhollyer@gmail.com>
2021-05-24 08:44:25 -0500

    change when to update # of entries

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const moment = require('moment');
 const createJob = require('./lib/cron');
+const { checkExpectedEntries } = require('./lib');
 
 if (
   moment().format('dddd') === 'Saturday' ||
   moment().format('dddd') === 'Sunday'
 ) {
+  checkExpectedEntries();
   console.log(`🤖 - Today is robot's day off!! ☕`);
   process.exit();
 } else {
   // eslint-disable-next-line global-require
   const bot = require('./auto');
   bot.on('start', () => {
-    bot.postMessageToChannel('our-marriage', '🤖 AlertBot starting...');
+    bot.postMessageToChannel('random', '🤖 AlertBot starting...');
     createJob();
   });
 }
