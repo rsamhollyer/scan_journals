434c272594c70095bf5184251daa958a4ccea64f
Sam Hollyer <rsamhollyer@gmail.com>
2021-06-01 08:14:36 -0500

    lazy comiit

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const moment = require('moment');
 const createJob = require('./lib/cron');
 const { checkExpectedEntries } = require('./lib');
 
-if (
-  moment().format('dddd') === 'Saturday' ||
-  moment().format('dddd') === 'Sunday'
-) {
-  checkExpectedEntries();
-  console.log(`🤖 - Today is robot's day off!! ☕`);
-  process.exit();
-} else {
-  // eslint-disable-next-line global-require
-  const bot = require('./auto');
-  bot.on('start', () => {
-    bot.postMessageToChannel('random', '🤖 AlertBot starting...');
-    createJob();
-  });
+async function startBot() {
+  if (
+    moment().format('dddd') === 'Saturday' ||
+    moment().format('dddd') === 'Sunday'
+  ) {
+    await checkExpectedEntries();
+    console.log(`🤖 - Today is robot's day off!! ☕`);
+    process.exit();
+  } else {
+    // eslint-disable-next-line global-require
+    const bot = await require('./auto');
+    bot.on('start', async () => {
+      bot.postMessageToChannel('random', '🤖 AlertBot starting...');
+      const result = await createJob();
+      return result;
+    });
+  }
 }
+
+startBot();
