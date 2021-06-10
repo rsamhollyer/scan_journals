58bb0736c6b1477fc96b726541cbfb6fa207a18b
Sam Hollyer <rsamhollyer@gmail.com>
2021-06-10 07:50:43 -0500

    fixed message receiver && gitpush

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ async function startBot() {
     moment().format('dddd') === 'Saturday' ||
     moment().format('dddd') === 'Sunday'
   ) {
-    await checkExpectedEntries();
     console.log(`🤖 - Today is robot's day off!! ☕`);
     process.exit();
   } else {
     // eslint-disable-next-line global-require
-    const bot = await require('./auto');
+    const bot = require('./auto');
     bot.on('start', async () => {
-      bot.postMessageToChannel('random', '🤖 AlertBot starting...');
-      const result = await createJob();
+      bot.postMessageToUser('rsamhollyer', '🤖 AlertBot starting...', {
+        disconnect: true,
+      });
+      await createJob();
     });
   }
 }
